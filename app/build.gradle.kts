32124a023df898a572efd63355927f04a004bd7d
Ika Riche <ikariche@gmail.com>
2026-03-06 23:03:50 +0200

    chore: bump versionCode to 88 for v0.8.8 (monotonic check fix)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kilu.pocketagent"
         minSdk = 26
         targetSdk = 34
-        versionCode = 87
-        versionName = "0.8.7"
+        versionCode = 88
+        versionName = "0.8.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
